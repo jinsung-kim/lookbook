c8ca857920daf615da358c17ea651a12cca3b8b5
Jin Kim <jinsungkim1210@gmail.com>
2020-04-25 11:31:31 -0700

    feat: home page set up

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 
+import "../Styles/Home.css";
+
 export default class Home extends Component {
     render() {
         return (
             <div className="home-wrapper">
                 <h1>Lookbook</h1>
                 <h2>A virtual curated closet</h2>
-                <h4>Why Lookbook?</h4>
-                <p>Skate, art, LA, NYC, fashion goes beyond</p>
-                <p>A means to eliminate fast fashion, and the constant need to buy</p>
-                <p>When we see something on sale, our immediate instinct is to purchase</p>
-                <p>We end up with a closet in which we barely touch a lot of what we consume</p>
-                <p>Lookbook encourages sustainable fashion, and mindful purchasing</p>
-                <p>Experiment with how colors collide with each other</p>
-                <p>Use our algorithm to determine what defines you</p>
-                <p>Whether it's form or functionality or trying to find that perfect piece</p>
-                <p>Break some rules, redefine what fashion means to you</p>
+                <div className="grid-container">
+                    <div className="grid-item">
+                        <h4>What is Lookbook?</h4>
+                        <p>Lookbook is an application built on helping users explore fashion and find inspiration.</p>
+                        <p>It is based on adding pieces to a closet, in which you can match with other pieces to create an outfit or test new colors.</p>
+                    </div>
+                    <div className="grid-item">
+                        <p>Image goes here</p>
+                    </div>
+                    <div className="grid-item">
+                        <p>Image goes here</p>
+                    </div>
+                    <div className="grid-item">
+                        <h4>Why Use Lookbook?</h4>
+                        <p>Lookbook is an application built on helping users explore fashion and find inspiration.</p>
+                        <p>It is based on adding pieces to a closet, in which you can match with other pieces to create an outfit or test new colors.</p>
+                    </div>
+                    <div className="grid-item">
+                        <h4>Philosophy</h4>
+                        <p>A means to reduce the impact of fast fashion, and the constant need to buy.</p>
+                        <p>When we see something on sale, our immediate instinct is to purchase.</p>
+                        <p>We end up with a closet in which we over consume.</p>
+                        <p>Lookbook encourages sustainable fashion, and mindful purchasing, by pushing users to think deeper about what they want.</p>
+                    </div>
+                    <div className="grid-item">
+                        <p>Image goes here</p>
+                    </div>
+                </div>
             </div>
         );
     }
