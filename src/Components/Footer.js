8d3b808680aefbb6d0c838832fddc899d02715fb
Jin Kim <jinsungkim1210@gmail.com>
2020-04-25 12:06:23 -0700

    feat: added ig tag at the end of the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-// import "../Styles/Footer.css";
 
 export default class Footer extends Component {
     render() {
         return (
             <div className="footer-wrapper">
                 <p>Created by Jin Kim</p>
-                {/* <Links /> */}
+                <p>@jinandjuiice</p>
             </div>
         );
     }
